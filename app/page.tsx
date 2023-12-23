ad9844f011901b566464715d5b5085c7f2b2077f
Suleyman <suley.kiani@gmail.com>
2023-12-22 20:10:01 -0500

    Removed Logic for sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,19 @@
-'use client'
-import { useEffect, useState } from 'react';
-import Image from 'next/image';
+// /app/page.ts
+import Topsongs from './tracks/page'
+import Topartists from './artists/page';
 
 export default function Home() {
- const [tracks, setTracks] = useState([]);
 
- useEffect(() => {
-   fetch('/api/topTracks')
-     .then((response) => response.json())
-     .then((data) => setTracks(data));
- }, []);
 
  return (
-   <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     {tracks.map((track, index) => (
-       <div key={index}>
-         <h2>{track.title}</h2>
-         <p>{track.artist}</p>
-         <a href={track.url}>Listen on Spotify</a>
-         <img src={track.coverImage.url} alt={track.title} width={300} height={300}/>
-         {/* <Image src={track.coverImage.url} alt={track.title} width={300} height={300} /> */}
-       </div>
-     ))}
-   </main>
+  <div className='container'>
+    {/* <Topsongs/> */}
+    {/* <Topartists/> */}
+  </div>
  );
+
 }
+
 
 
 
